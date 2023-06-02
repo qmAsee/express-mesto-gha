10ad498460c49e1b907a8f19f830efc6c63e96b5
Maxim Kupchin <max.kup.gis@yandex.ru>
2023-06-02 21:38:33 +0500

    2nd rev, fixed errors

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes/routers');
+const { NOT_FOUND } = require('./utils/responses');
 
 const {
   PORT = 3000,
-  MONGO_URL = 'mongodb://127.0.0.1/mestodb'
+  MONGO_URL = 'mongodb://127.0.0.1/mestodb',
 } = process.env;
 mongoose.connect(MONGO_URL);
 
 const app = express();
 
 app.use(express.json());
+app.use((req, res) => {
+  res.status(NOT_FOUND).send(req);
+});
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '6475ca24b169212a8b667885'
+    _id: '6475ca24b169212a8b667885',
   };
 
   next();
@@ -23,10 +27,5 @@ app.use((req, res, next) => {
 app.use(router);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`);
 });
-
-
-
-
-
