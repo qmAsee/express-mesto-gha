f9e0e907b71ca2ef9e037f951161c1ee339254ba
Maxim Kupchin <max.kup.gis@yandex.ru>
2023-06-19 00:07:05 +0500

    weweq

--- routes/routers.js
+++ routes/routers.js
@@ -3,21 +3,8 @@ const userRoutes = require('./users');
 const cardsRouter = require('./cards');
 const NotFound = require('../utils/errorClasses/ErrorNotFound');
 
-const {
-  loginValidation,
-  createUserValidation,
-} = require('../middlewares/fieldsValidaton');
-
-const {
-  createUser,
-  login,
-} = require('../controllers/users');
-
 router.use('/users', userRoutes);
 router.use('/cards', cardsRouter);
-
-router.post('/signin', loginValidation, login);
-router.post('/signup', createUserValidation, createUser);
 
 router.use('*', (req, res, next) => {
   next(new NotFound('Страница не найдена'));
--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,17 @@ const {
   findUserByIdValidation,
   updateUserValidation,
   uploadAvatarValidation,
+  loginValidation,
+  createUserValidation,
 } = require('../middlewares/fieldsValidaton');
 
 const {
+  createUser,
   updateUser,
   findUserById,
   getUsers,
   uploadAvatar,
+  login,
   getCurrentUser,
 } = require('../controllers/users');
 
@@ -19,6 +23,8 @@ router.use(auth);
 router.patch('/me', updateUserValidation, updateUser);
 router.patch('/me/avatar', uploadAvatarValidation, uploadAvatar);
 router.get('/:userId', findUserByIdValidation, findUserById);
+router.post('/signin', loginValidation, login);
+router.post('/signup', createUserValidation, createUser);
 router.get('/', getUsers);
 router.get('/me', getCurrentUser);
 
